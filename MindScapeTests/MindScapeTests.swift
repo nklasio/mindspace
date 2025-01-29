06f9cf51fabe49864966e836a4177545443563a1
Niklas Stambor <niklas.stambor@nstambor.de>
2025-01-29 14:49:19 +0100

    Initial Commit

---
+++ MindScapeTests/MindScapeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MindScapeTests.swift
+//  MindScapeTests
+//
+//  Created by Niklas Stambor on 29.01.25.
+//
+
+import Testing
+
+struct MindScapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
